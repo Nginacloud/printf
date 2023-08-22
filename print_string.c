25f29027d3cb50c3e567476d59e1a4b406fde136
Nginacloud <nginadorcas451.gmail.com>
2023-08-22 13:36:26 +0000

    main.h

--- print_string.c
+++ print_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-void print_string(char *str, int *len)
+static void print_string(char *str, int *len)
 {
 	if (str == NULL)
 		str = "(null)";
+
 	while (*str != '\0')
 	{
 		_putchar(*str);
