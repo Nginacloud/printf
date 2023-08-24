8575b35941b55139579143b47d7b82f59038563a
Nginacloud <nginadorcas451.gmail.com>
2023-08-24 21:35:17 +0000

    got print number to normal state

--- print_number.c
+++ print_number.c
@@ -3,28 +3,7 @@
 #include <limits.h>/*to handle INT MIN*/
 void print_number(int number)
 {
-	if (number == INT_MIN)
-	{
-		_putchar('_');
-		print_number(1);
-		print_number(214748364);
-	}
-	else if (number < 0)
-	{
-		_putchar('_');
-		print_number(-number);
-	}
-	else if (number >= 10)
-	{
-		print_number(number / 10);
-		_putchar(number % 10 + '0');
-	}
-	else
-	{
-		_putchar(number + '0');
-	}
-
-	/*if (number < 0)
+	if (number < 0)
 	{
 		putchar('-');
 		number = number * -1;
@@ -44,5 +23,5 @@ void print_number(int number)
 		number = number % divisor;
 		divisor = divisor / 10;
 		*len = *len + 1;
-		}*/
+		}
 }
