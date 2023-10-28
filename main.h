d534938068fff0a144b09fad98ef5fc5a83bb4f6
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-10-28 20:42:21 +0300

    I changed the prototype of print binary
    I send an unsigned number to print binary
    insted of sending a signed number

--- main.h
+++ main.h
@@ -1,9 +1,17 @@
 #ifndef MAIN_H
-# define MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 void print_string(char *str, int *len);
 void print_number(int, int *);
-void print_binary(int, int *);
+void print_binary(unsigned int, int *);
 #endif
