973581c2e1ee1717be3ee318e2dfeb3a1dac76a2
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-10-28 20:41:13 +0300

    changed the working of print binary
    I now use a string to print binary
    I store all the digits in a string
    then print the string

--- print_binary.c
+++ print_binary.c
@@ -1,28 +1,43 @@
 #include "main.h"
-#include <stdio.h>
-/**
-  *print_binary - takes a number and gets its binary 
-  *number then prints its like a string
-  *@number: the number to print 
-  *@len: how far the length has come
-  *
-  *Return: nothing 
-  */
-void print_binary(int number, int *len)
-{
-	int bit = 0;
-	int multiplier = 1;
-	int binary_number = 0;
-	int bit_offset;
 
+/**
+ *print_binary - takes a number and gets its binary
+ *number then prints its like a string
+ *@number: the number to print
+ *@len: how far the length has come
+ *
+ *Return: nothing
+ */
+void print_binary(unsigned int number, int *len)
+{
+	unsigned int numcpy = 0;
+	int i = 0;
+	char *numStr = NULL;
+
+	numcpy = number;
+	while (numcpy > 0)
+	{
+		numcpy = numcpy / 2;
+		i++;
+	}
+	numStr = malloc(sizeof(char) * i);
+	if (numStr == NULL)
+	{
+		return;
+	}
+	numStr[i] = '\0';
+	i--;
 	while (number > 0)
 	{
-		bit = number % 2;
-		bit_offset = bit * multiplier;
-		binary_number = binary_number + bit_offset;
-		multiplier = multiplier * 10;
+		numStr[i] = (number % 2) + '0';
+		i--;
 		number = number / 2;
 	}
-	/*printf("%d", binary_number);*/
-	print_number (binary_number, len);
+	i = 0;
+	while (numStr[i] != '\0')
+	{
+		_putchar(numStr[i]);
+		(*len)++;
+		i++;
+	}
 }
